02475e41685242b828ba00ebfa0170f2007272ae
Camilla Ligovic <camilla.ligovic@gmail.com>
2023-11-24 13:26:01 +0100

    created feature to upload product

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @items = Item.all
     if params[:category].present?
@@ -19,5 +21,32 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     # more logic needed?
+  end
+
+  # Added methods
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if current_user
+      @item.user = current_user
+      if @item.save
+        redirect_to user_path(current_user)
+      else
+        render :new, status: 422
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :cover)
+    # permit category id/user id?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     # @user = User.find(params[:id])
   end
